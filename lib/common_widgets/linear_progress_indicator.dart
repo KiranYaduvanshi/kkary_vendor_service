659a4a442e513dc81da715d1ae7969e592fdc794
s.rana1 <s.rana@rowthtech.com>
2022-03-03 16:52:28 +0530

    First log

---
+++ lib/common_widgets/linear_progress_indicator.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ProgressIndecator{
+
+  static LinearProgressIndicator _linearProgressIndicator({required String Text}){
+    return LinearProgressIndicator();
+
+  }
+}
---
+++ lib/routes/app_pages.dart
@@ -0,0 +1,16 @@
+import 'package:get/get.dart';
+import 'package:kkary_vendors/modules/services_vendor/views/IntroScreens/partner.dart';
+import 'package:kkary_vendors/routes/app_routes.dart';
+class AppPages {
+
+  static const INITIAL = AppRoutes.PARTNER;
+
+  //  static const INITIAL = Routes.WELCOME_CUSTOMER_SCREEN;
+  static final routes = [
+    GetPage(
+      name: AppRoutes.PARTNER,
+      page: () =>const Partner(),
+    ),
+
+  ];
+}
